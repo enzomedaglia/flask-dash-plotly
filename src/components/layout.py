140ab3a72ef8ff638bd067661eae6afcff976ea2
Enzo Medaglia <enzomedaglia@gmail.com>
2022-10-17 23:17:24 -0300

    Update layout.py

--- src/components/layout.py
+++ src/components/layout.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 
 """
@@ -26,24 +25,4 @@ def create_layout(app: Dash, data: pd.DataFrame) -> html.Div:
             bar_chart.render(app, data),
             pie_chart.render(app, data),
         ]
-=======
-from dash import Dash, html
-
-from . import bar_chart, nation_dropdown
-
-def create_layout(app: Dash) -> html.Div:
-    return html.Div(
-        className="app-div",
-        children=[
-            html.H1(app.title),
-            html.Hr(),
-            html.Div(
-                className="dropdown-container",
-                children=[
-                    nation_dropdown.render(app)
-                ]
-            ),
-            bar_chart.render(app)
-        ]
->>>>>>> 03d50657ce781f0edd070cb95879d1afe512d424
-    )
+        
